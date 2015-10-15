ac00832fef32834a6fe54616d63d18b859871469
Prasenjit Purohit <github@prasenjit.net>
2015-10-16 03:05:02 +0530

    controller uncommented

--- src/test/java/com/example/TestgroundApplicationTests.java
+++ src/test/java/com/example/TestgroundApplicationTests.java
@@ -1,20 +1,18 @@
 package com.example;
 
-import junit.framework.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TestgroundApplication.class)
 @WebAppConfiguration
 public class TestgroundApplicationTests {
 
-	@Test
-	public void contextLoads() {
-		Assert.fail();
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
