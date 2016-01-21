ef345d335b0f88e33bfaee7b5bef82f15293dee5
prasenjit-net <github@prasenjit.net>
2016-01-22 01:41:31 +0530

    test fail

--- src/test/java/com/example/TestgroundApplicationTests.java
+++ src/test/java/com/example/TestgroundApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import static org.junit.Assert.fail;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TestgroundApplication.class)
 @WebAppConfiguration
@@ -15,7 +13,6 @@ public class TestgroundApplicationTests {
 
     @Test
     public void contextLoads() {
-        fail("not implemented");
     }
 
 }
