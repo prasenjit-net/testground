fb27b54fd0d7f27e44a550c17b04da7b510e6f19
prasenjit-net <github@prasenjit.net>
2016-01-27 13:53:20 +0530

    test base class added

---
+++ src/test/java/com/example/controller/AbstractControllerBase.java
@@ -0,0 +1,43 @@
+package com.example.controller;
+
+import com.example.TestgroundApplication;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.restdocs.RestDocumentation;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Properties;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+/**
+ * Created by PRASEN on 1/27/2016.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TestgroundApplication.class)
+@WebAppConfiguration
+@Ignore
+public class AbstractControllerBase {
+    @Rule
+    public RestDocumentation restDocumentation = new RestDocumentation(System.getProperty("buildDirectory") + "/snippets");
+
+    @Autowired
+    protected WebApplicationContext context;
+
+    protected MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = webAppContextSetup(context).apply(documentationConfiguration(restDocumentation)).build();
+        Properties buildDirectory = System.getProperties();
+        System.out.println(buildDirectory);
+    }
+}
---
+++ src/test/java/com/example/controller/PersonControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import org.junit.Test;
+
+/**
+ * Created by PRASEN on 1/27/2016.
+ */
+public class PersonControllerTest extends AbstractControllerBase {
+
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+
+    @Test
+    public void testView() throws Exception {
+
+    }
+
+    @Test
+    public void testOrders() throws Exception {
+
+    }
+}
